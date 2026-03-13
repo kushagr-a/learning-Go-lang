79cd821864734fa7b4becaa17526009a19f66394
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-03-13 21:46:03 +0530

    feat: add Go defer keyword demonstration including LIFO execution and resource cleanup examples.

--- defer/main.go
+++ defer/main.go
@@ -9,7 +9,7 @@ func main() {
 
 	// LIFO (Last In First Out)
 
-	// The defer statement in Go is used to postpone the execution of a function 
+	// The defer statement in Go is used to postpone the execution of a function
 	// until the surrounding function returns
 
 	defer fmt.Println("one")
@@ -17,6 +17,16 @@ func main() {
 	defer fmt.Println("three")
 
 	fmt.Println("hello")
+
+	fmt.Println("case 1: success")
+	if err := dowork(true); err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println("case 2: failure")
+	if err := dowork(false); err != nil {
+		fmt.Println("error:", err)
+	}
 
 	// output: hello three two one
 
@@ -27,4 +37,23 @@ func main() {
 
 	// example
 
+}
+
+func dowork(success bool) error {
+	// resorce related
+	// start message -> resource acquired
+	// clenup
+
+	fmt.Println("start: resource aquire")
+
+	// defer will guarenteed to run even if function returns early
+	// success or failure
+	defer fmt.Println("cleanup: resource released")
+
+	if !success {
+		return fmt.Errorf("failed to aquire resource")
+	}
+
+	fmt.Println("success: resource used")
+	return nil
 }
